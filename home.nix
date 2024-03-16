4b366d46354dad21b5c5c99d1315596ad949f643
JaydenPahukula <jayden.pahukula@gmail.com>
2024-03-16 00:07:23 -0600

    added discord and spotify (and allow unfree packages)

--- home.nix
+++ home.nix
@@ -15,12 +15,14 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages =  [
+    pkgs.discord
+    pkgs.spotify
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
