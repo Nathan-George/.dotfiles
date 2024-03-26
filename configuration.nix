91f92236c919df0fa604db8bdcb991e15898d7de
JaydenPahukula <jayden.pahukula@gmail.com>
2024-03-25 18:20:43 -0600

    add automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # automatic garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
