10c0174df80d21e8425d7f2bc8e2a0499a1c6bd7
JaydenPahukula <jayden.pahukula@gmail.com>
2024-03-29 15:52:26 -0600

    scripts module touchups

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,24 +1,24 @@
 # This file stores all scripts in the nix-store and makes their
 # paths available under `options.scripts`.
 
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 with lib; {
 
-    # declare home manager option
-    options.scripts = mkOption {
-        type = types.attrsOf types.path;
-        default = {};
-        example = literalExpression { myScript = "./myscript.sh"; };
-        description = "A collection of scripts and their paths in
-        the nix store.";
-    };
+  # declare home manager option
+  options.scripts = mkOption {
+    type = types.attrsOf types.path;
+    default = {};
+    example = literalExpression { myScript = "./myscript.sh"; };
+    description = "A collection of scripts and their paths in
+    the nix store.";
+  };
 
-    # define scripts
-    config.scripts = {
-        appLauncher = ./applauncher.sh;
-        brightnessNotification = ./brightnessnotification.sh;
-        volumeNotification = ./volumenotification.sh;
-    };
+  # define scripts
+  config.scripts = {
+    appLauncher = ./applauncher.sh;
+    brightnessNotification = ./brightnessnotification.sh;
+    volumeNotification = ./volumenotification.sh;
+  };
 
 }
