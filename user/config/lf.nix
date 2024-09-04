d9dcbb2125e41a2351363e044c9e1e61d598d930
JaydenPahukula <jayden.pahukula@gmail.com>
2024-09-04 17:11:57 -0600

    lf stuff

--- user/config/lf.nix
+++ user/config/lf.nix
@@ -39,7 +39,7 @@
       unzip = ''''${{
         case "$f" in
           *.zip) ${pkgs.unzip}/bin/unzip "$f" ;;
-          *.tar.gz) tar -xzvf "$f" ;;
+          *.tar.gz|*.tgz) tar -xzvf "$f" ;;
           *.tar.bz2) tar -xjvf "$f" ;;
           *.tar) tar -xvf "$f" ;;
           *) echo "Unsupported archive format" ;;
