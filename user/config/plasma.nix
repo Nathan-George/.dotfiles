9a6cc4f7d8236af10bf2e4ed1f66fcbc66044261
JaydenPahukula <jayden.pahukula@gmail.com>
2024-09-05 23:26:04 -0600

    add display config

--- user/config/plasma.nix
+++ user/config/plasma.nix
@@ -1,11 +1,20 @@
 # plasma config
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.plasma = {
     enable = true;
     overrideConfig = true;
+
+    # setup displays on startup
+    startup.startupScript."displaySetup".text = ''
+      ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor \
+        output.1.scale.1.3 \
+        output.1.position.0,0 \
+        output.2.scale.1 \
+        output.2.position.-1920,0 \
+    '';
 
     input.mice = [
       {
