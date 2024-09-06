9169d96d4130e008d11dc48af7944b62d3f4a52c
JaydenPahukula <jayden.pahukula@gmail.com>
2024-09-05 22:56:20 -0600

    add mouse config

--- user/config/plasma.nix
+++ user/config/plasma.nix
@@ -6,5 +6,17 @@
   programs.plasma = {
     enable = true;
     overrideConfig = true;
+
+    input.mice = [
+      {
+        acceleration = -0.2;
+        accelerationProfile = "none";
+        enable = true;
+        name = "Razer Razer DeathAdder Essential";
+        naturalScroll = false;
+        productId = "006e";
+        vendorId = "1532";
+      }
+    ];
   };
 }
