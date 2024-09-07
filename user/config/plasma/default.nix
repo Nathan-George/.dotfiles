d9dde1affa29ec45c4ce3ed4399addffe1d904ac
JaydenPahukula <jayden.pahukula@gmail.com>
2024-09-06 23:06:04 -0600

    move display config to its own file

--- user/config/plasma/default.nix
+++ user/config/plasma/default.nix
@@ -1,6 +1,6 @@
 # plasma config entrypoint
 
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
@@ -10,15 +10,5 @@
   programs.plasma = {
     enable = true;
     overrideConfig = true;
-
-    # setup displays on startup
-    startup.startupScript."displaySetup".text = ''
-      ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor \
-        output.1.scale.1.3 \
-        output.1.position.0,0 \
-        output.2.scale.1 \
-        output.2.position.-1920,0 \
-    '';
-
   };
 }
---
+++ user/config/plasma/display.nix
@@ -0,0 +1,13 @@
+# plasma startup script for setting up displays
+
+{ pkgs, ... }:
+
+{
+  programs.plasma.startup.startupScript."displaySetup".text = ''
+    ${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor \
+      output.1.scale.1.3 \
+      output.1.position.0,0 \
+      output.2.scale.1 \
+      output.2.position.-1920,0 \
+  '';
+}
