205c55776736763792a6ae9322d0ee5d179d183d
JaydenPahukula <jayden.pahukula@gmail.com>
2024-10-16 12:32:22 -0600

    basic mako theme

--- home/config/mako.nix
+++ home/config/mako.nix
@@ -1,10 +1,16 @@
 # mako config
 
-{ ... }:
+{ config, ... }:
 
 {
   services.mako = {
     enable = true;
     defaultTimeout = 3000;
+
+    extraConfig = with config.colors; ''
+      background-color=#${base00}
+      border-color=#${base07}
+      text-color=#${base07}
+    '';
   };
 }
