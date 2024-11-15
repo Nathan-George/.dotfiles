f1f43ca94cc036f21b46d88726f2abe5ae97dcd8
JaydenPahukula <jayden.pahukula@gmail.com>
2024-11-14 22:04:34 -0700

    fix wireshark

--- system/config/wireshark.nix
+++ system/config/wireshark.nix
@@ -1,6 +1,6 @@
 # wireshark config
-{...}: {
-  # enable wireshark
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [wireshark];
   programs.wireshark.enable = true;
 
   # add user to wireshark group
