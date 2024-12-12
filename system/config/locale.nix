abb560da9818e42a83c7cba57cb1fb368954c56f
JaydenPahukula <jayden.pahukula@gmail.com>
2024-12-12 09:56:01 -1000

    update time zone

--- system/config/locale.nix
+++ system/config/locale.nix
@@ -1,5 +1,6 @@
 # region and locale
 {...}: {
-  time.timeZone = "America/Denver";
+  # time.timeZone = "America/Denver";
+  time.timeZone = "Pacific/Honolulu";
   i18n.defaultLocale = "en_US.UTF-8";
 }
