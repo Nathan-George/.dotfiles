5c4025361d0e8b399d63257368cd2509a2cf0a42
Nathan George <nbg0520@gmail.com>
2025-08-25 22:26:22 -0600

    added license file

--- home/profiles/school.nix
+++ home/profiles/school.nix
@@ -3,4 +3,12 @@
     globalprotect-openconnect
     quartus-prime-lite
   ];
+
+  home.sessionSearchVariables = let
+    quartus-license-file = ./LR-247745_License.dat;
+  in {
+    LM_LICENSE_FILE = [
+      quartus-license-file
+    ];
+  };
 }
